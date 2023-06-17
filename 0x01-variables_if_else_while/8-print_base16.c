c8190016705b9040171e4bf242a86dd1914f5956
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-16 17:04:12 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,12 @@
-i/**
+ /**
  * main - entry point
  * prints all numbers of base 16 decimal.
- *
  * Return: 0 on (success)
  */
-
 #include <stdio.h>
 int main(void)
 {
+
 	int i = '0';
 
 	while (i <= '9')
