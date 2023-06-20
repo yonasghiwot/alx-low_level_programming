80bd492bf90a9a9d6d99f266a3bf9b29032e6a8f
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-20 13:34:11 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: the int to check
  *
  * Return: 1 and prints + if n is greater than zero
- * Return: 0 and prints 0 if n is zero
- * Retutn : -1 and prints - if n is less than zero
+ * 0 and prints 0 if n is zero
+ * 1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
