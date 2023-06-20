fbd127edd48070c719bb9bc556ef34bed2dc7fec
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-20 15:46:43 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,10 +6,9 @@
  *
  * Return: ALways 0 (Success)
  */
-int main(void)
 {
-	unsigned long int i, j, k;
-        unsigned long int j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
+
 	j = 1;
 	k = 2;
 
@@ -28,9 +27,8 @@ int main(void)
 	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
-	{
-		k2 = k2 / 1000000000;
-		printf(", %lu", k1 + k2 );
+	{       
+		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
