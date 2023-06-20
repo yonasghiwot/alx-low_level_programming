cf3d9457074952d06a09ff624932bd45e1f2d7f2
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-20 15:49:08 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,10 +2,11 @@
 
 /**
  * main - finds and prints the first 98 Fibonacci numbers,
- * starting with 1 and 2 followed by a new line
- *
+ * starting with 1 and 2
+ * followed by a new line
  * Return: ALways 0 (Success)
  */
+int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
 
@@ -27,7 +28,7 @@
 	k2 = k % 1000000000;
 
 	for (i = 92; i < 99; ++i)
-	{       
+	{
 		printf(", %lu", k1 + (k2 / 1000000000));
 		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
