4e47f99cf6a5ee6e52b7849936c55d1d7f45d523
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-21 05:43:53 -0700

    correction betty code style

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
-* main - test that the function positive_or_negative() gives the correct output when given a case of 0.
-* Return: 0
+* main - test that the function positive_or_negative()
+* gives the correct output when given a case of 0.
+* @i: is the test number
+*
+* Return: 0 (on succes)
 */
 int main(void)
 {
