20ace2e0db2c47c17511e43058e6ab60e12f2563
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-22 06:07:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers-function that prints the numbers 0 to 9, followed by a new line.
+ * Description: Prints the numbers with _putchar using for loop
+ *
+ * Return: No return
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
