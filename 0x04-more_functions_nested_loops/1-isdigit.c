b962b155ce4a713be1e1929da8917604e79c63d9
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-22 05:41:04 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit defines if a character is a number
+ * @c: charater
+ * Return: 1 true 0 false 
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
