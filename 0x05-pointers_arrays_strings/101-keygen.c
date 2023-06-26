947b2ffdb9a43cf80d85428a727b395092e2e22a
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-26 11:36:30 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+  * main - creates a random key for the program 101 crackme
+  * Return: always 0
+  */
+int main(void)
+{
+	    int sum, i, r;
+
+		char decode[27] = "abcdefghijklmnopqrstuvwxyz";
+		char key[30];
+
+		sum = 0;
+		i = 0;
+
+		srand(time(NULL));
+
+		while (sum < 2772)
+		{
+			r = rand() % 10;
+			key[i] = decode[r];
+			sum += key[i];
+			i++;
+		}
+		r = 2772 - sum;
+		key[i] = r;
+		printf("%s\n",  key);
+		return (0);
+}
