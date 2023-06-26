02edbce3604299a9579bc1ded0c657c66a2f33c7
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-26 10:08:23 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * puts_half - Prints the last half of a string.
+ * @str: input string
+ * Return: nothing 
+ */
+void puts_half(char *str)
+{
+	int i, j, length, halflenght;
+
+	i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	length = i;
+	if (length % 2 == 0)
+	{
+		halflength= length / 2;
+	}
+	else
+	{
+		halflength = (length - 1) / 2;
+	}
+
+	j = length - halflength;
+
+	while (*(str + j) != '\0')
+	{
+		_putchar(*(str + j));
+		j++;
+	}
+	_putchar('\n');
+}
