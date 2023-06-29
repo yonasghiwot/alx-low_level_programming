80b62869b6f6ce3c26fa627a0633db3b7e420a91
yonasghiwot <yonihabeshaw@gmail.com>
2023-06-29 05:49:13 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,23 +10,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int dest_len = 0;
-	int  i;
-
 	int src_len = 0;
 
 	while (*(dest + dest_len) != '\0')
 	{
 		dest_len++;
 	}
-	dest_len = dest_len - 1;
-	while (*(src + src_len) != '\n')
+	while (*(src + src_len) != '\0' && dest_len < 97)
 	{
+		*(dest + dest_len) = *(src + src_len);
+		dest_len++;
 		src_len++;
 	}
-	src_len = src_len - 1;
-	for (i = 0 ; *(src + i) != '\0' ; i++)
-	{
-		*(dest + dest_len + i + 1) = *(src + i);
-	}
+	*(dest + dest_len) = '\0';
 	return (dest);
 }
