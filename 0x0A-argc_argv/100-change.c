3aaf2d6c5ff477d08254f5d9135812f0292cb756
yonasghiwot <yonihabeshaw@gmail.com>
2023-07-07 15:28:35 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to print out the change that needs to given
+ * @argc: number of arguments to be passed to the program
+ *  @argv: pointer to list of pointers to strings
+ *  Return :1 a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char **argv)
+{
+	int total, change;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	change = atoi(argv[1]);
+
+	for (total = 0; change > 0; total++)
+	{
+		if (change - 25 >= 0)
+			change = change - 25;
+		else if (change - 10 >= 0)
+			change = change - 10;
+		else if (change - 5 >= 0)
+			change = change - 5;
+		else if (change - 2 >= 0)
+			change = change - 2;
+		else if (change - 1 >= 0)
+			change = change - 1;
+	}
+	printf("%d\n", total);
+	return (0);
+}
