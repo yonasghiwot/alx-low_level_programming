ca2e6cc74942d7102284dbd071e26c3644d5faa9
yonasghiwot <yonihabeshaw@gmail.com>
2023-08-04 16:28:34 +0300

    100-reverse_listint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * reverse_listint - func reverses order of a listint_t singly linked list
+ * @head: double pointer to the first element of a singly linked 
+ * list of structs of type listint_s
+ * Return: the head of reversed list, or NULL if failed
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *temp;
+
+	if (!head || !*head)
+		return (NULL);
+
+	while ((*head)->next)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		temp->next = prev;
+		prev = temp;
+	}
+	(*head)->next = prev;
+
+	return (*head);
+}
