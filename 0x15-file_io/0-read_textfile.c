c32caabdf036266b75b8bc736cdf764ef7e00872
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-05 01:53:17 +0300

    inserting descreprtion for function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/*
- * read_textfile - reads a text file and prints the letters
+/**
+ * read_textfile - reads a character filename and prints the letters
  * @filename: filename
  * @letters: numbers of letters printed
  * Return: numbers of letters printed. It fails, returns 0
