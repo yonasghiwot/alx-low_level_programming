93d27025e873ac7a2c537fdb9ea183969aa95d92
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-05 01:45:04 +0300

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
-/**
+/*
  * read_textfile - reads a text file and prints the letters
  * @filename: filename
- * @letters: numbers of letters printed.
- * Return: numbers of letters printed. It fails, returns 0.
+ * @letters: numbers of letters printed
+ * Return: numbers of letters printed. It fails, returns 0
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
 	char *buf;
-
+	
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
