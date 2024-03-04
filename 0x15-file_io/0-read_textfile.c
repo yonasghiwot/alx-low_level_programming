db4760485314bb5bf42993be0cef87d426607e6b
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-05 01:56:22 +0300

    inserting descreprtion for function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,10 +1,11 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a character filename and prints the letters
- * @filename: filename
- * @letters: numbers of letters printed
- * Return: numbers of letters printed. It fails, returns 0
- */
+* read_textfile - reads a character filename and prints the letters
+* @filename: filename
+* @letters: numbers of letters printed
+* Return: numbers of letters printed. It fails, returns 0
+*/
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
