91f6717d0a899fcaf2804951bcbcfb07eb690553
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-05 02:02:27 +0300

    bettystylecoding

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,18 +13,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 	
-	if (!filename)
+	if (!filename){
 		return (0);
+	}
+	
 	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	
+	if (fd == -1){
 		return (0);
+	}
+	
 	buf = malloc(sizeof(char) * (letters));
 	
-	if (!buf)
+	if (!buf){
 		return (0);
+	}
+	
 	nrd = read(fd, buf, letters);
 	nwr = write(STDOUT_FILENO, buf, nrd);
+	
 	close(fd);
 	free(buf);
 	return (nwr);
