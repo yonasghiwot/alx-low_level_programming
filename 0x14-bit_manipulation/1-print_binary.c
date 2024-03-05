92c15f97ff1667b3d766ff9c49e3136c6a1cc691
yonasghiwot <yonihabeshaw@gmail.com>
2024-03-05 06:12:36 -0800

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 
 /**
@@ -14,18 +13,4 @@ void print_binary(unsigned long int n)
 	_putchar((n & 1) + '0);
 
 }
-=======
-#include "main.h"
 
-/*
- * print_binary:Prints the binary representation of a number
- * @n:The number to be printed in binary
- */
-
-void print_binary(unsigned long int n)
-{
-	if (n > 1)
-		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
-}
->>>>>>> 3f7f7683742bb1ba15feda0584f4bef4a157ef35
